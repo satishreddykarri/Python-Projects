4802aef14ee1e2504a47e677a34f0efc62e2b26d
satishreddykarri <satishreddykarri121@gmail.com>
2024-04-16 21:36:09 +0530

    This project is about the hungman game

---
+++ Hungman_Game.py
@@ -0,0 +1,28 @@
+import random
+import Hungmanstage
+word_list = ['apple','banana','potato']
+lives = 6
+chosen_word = random.choice(word_list)
+print(chosen_word)
+display = []
+for letter in chosen_word:
+    display += '_'
+print(display)
+game_over = False
+while not game_over:
+    guessed_letter = input("Guess a letter : ").lower()
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guessed_letter:
+            display[position] = guessed_letter
+    print(display)
+    if guessed_letter not in chosen_word:
+        lives -= 1
+        print(f"you have only {lives} chances left")
+        if lives == 0:
+            game_over = True
+            print("You Lose!!!")
+    if '_' not in display:
+        game_over = True
+        print("You winn!!!")
+    print(Hungmanstage.stages[lives])
